7e167313b371cbf0768f5f682e23ea59fbb281b6
Mark Jimenez <mark.jimenez@ucalgary.ca>
2024-03-27 14:19:06 -0600

    update

--- lab10/backend/venv/server.py
+++ lab10/backend/venv/server.py
@@ -3,3 +3,18 @@ import json
 import os
 
 app = Flask(__name__)
+
+@app.route('/api/v1/employees', methods=['GET'])
+@app.route('/prodcuts/<int:product_id>', methods=['GET'])
+def get_products(product_id=None):
+    if product_id:
+        with open('products.json') as f:
+            data = json.load(f)
+            for product in data:
+                if product['id'] == product_id:
+                    return jsonify(product)
+            return jsonify({'message': 'product not found'}), 404
+    else:
+        with open('products.json') as f:
+            data = json.load(f)
+            return jsonify(data)
